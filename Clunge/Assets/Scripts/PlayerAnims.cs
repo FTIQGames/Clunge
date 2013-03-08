a9d08d75e8cd0817b63a7802353d1bb3fb1d5dd8
David <ftiqgames@gmail.com>
2013-03-08 01:33:04 -0600

    Created Animated Sprite
    I have created an animated sprite in scene test2. There is an issue with
    movement as it moves very sluggish and jumps only slighlty and raising
    the value for the jumps results in a slow fall back down. Working on
    finding a solution to fix the issue.

--- Clunge/Assets/Scripts/PlayerAnims.cs
+++ Clunge/Assets/Scripts/PlayerAnims.cs
@@ -5,7 +5,7 @@ public class PlayerAnims : MonoBehaviour
 {
 public enum anim { None, WalkLeft, WalkRight, RopeLeft, RopeRight, Climb, ClimbStop, StandLeft, StandRight, HangLeft, HangRight, FallLeft, FallRight , ShootLeft, ShootRight }
 	
-	public Transform spriteParent;
+	//public Transform spriteParent;
 	public tk2dAnimatedSprite playerSprite;
 	
 	private anim currentAnim;
@@ -24,27 +24,31 @@ public enum anim { None, WalkLeft, WalkRight, RopeLeft, RopeRight, Climb, ClimbS
 		{
 			currentAnim = anim.WalkLeft;
 			playerSprite.Play("run");
-			spriteParent.localScale = new Vector3(1,1,1);
+			//spriteParent.localScale = new Vector3(1,1,1);
+			//playerSprite.transform.localScale = new Vector3(1,1,1);
 		}
 		if(!player.isLeft && player.grounded == true && currentAnim != anim.StandLeft && player.facingDir == Player.facing.Left)
 		{
 			currentAnim = anim.StandLeft;
 			playerSprite.Play("stand"); // stand left
-			spriteParent.localScale = new Vector3(1,1,1);
+			//spriteParent.localScale = new Vector3(1,1,1);
+			//playerSprite.transform.localScale = new Vector3(1,1,1);
 		}
 		
 		// run right
 		if(player.isRight && player.grounded && currentAnim != anim.WalkRight)
 		{
 			currentAnim = anim.WalkRight;
-			playerSprite.Play("run");
-			spriteParent.localScale = new Vector3(-1,1,1);
+			playerSprite.Play("runRight");
+			//spriteParent.localScale = new Vector3(-1,1,1);
+			//playerSprite.transform.localScale = new Vector3(-1,1,1);
 		}
 		if(!player.isRight && player.grounded && currentAnim != anim.StandRight && player.facingDir == Player.facing.Right)
 		{
 			currentAnim = anim.StandRight;
-			playerSprite.Play("stand"); // stand left
-			spriteParent.localScale = new Vector3(-1,1,1);
+			playerSprite.Play("standRight"); // stand right
+			//spriteParent.localScale = new Vector3(-1,1,1);
+			//playerSprite.transform.localScale = new Vector3(-1,1,1);
 		}
 		
 		// falling
@@ -52,13 +56,15 @@ public enum anim { None, WalkLeft, WalkRight, RopeLeft, RopeRight, Climb, ClimbS
 		{
 			currentAnim = anim.FallLeft;
 			playerSprite.Play("jump"); // fall left
-			spriteParent.localScale = new Vector3(1,1,1);
+			//spriteParent.localScale = new Vector3(1,1,1);
+			//playerSprite.transform.localScale = new Vector3(1,1,1);
 		}
 		if(player.grounded == false && currentAnim != anim.FallRight && player.facingDir == Player.facing.Right)
 		{
 			currentAnim = anim.FallRight;
-			playerSprite.Play("jump"); // fall right
-			spriteParent.localScale = new Vector3(-1,1,1);
+			playerSprite.Play("jumpRight"); // fall right
+			//spriteParent.localScale = new Vector3(-1,1,1);
+			//playerSprite.transform.localScale = new Vector3(-1,1,1);
 		}
 	}
 }
